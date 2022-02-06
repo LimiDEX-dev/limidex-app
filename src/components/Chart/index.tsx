7aad5f2d0dcd5c3222a4944e6ec9fa1489bd5e85
SergeyBeltsin <beltsins89@gmail.com>
2022-02-06 13:56:00 +0300

    add chart header

--- src/components/Chart/index.tsx
+++ src/components/Chart/index.tsx
@@ -5,6 +5,41 @@ import './style.scss';
 export function Chart(props: any) {
   return (
     <div className="Chart">
+      <div className="chart__header">
+        <div className="chart__header__flex-wrapper">
+          <span className="chart__header__title">
+            Rates
+          </span>
+          <div className="chart__header__timeframe">
+            <span className="chart__header__timeframe__text">
+              Timeframe
+            </span>
+            <span className="chart__header__timeframe__wrapper">
+              <span className="chart__header__timeframe__text">
+                m
+              </span>
+              <span className="chart__header__timeframe__minutes">
+                10
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="chart__header__current">
+          <div className="chart__header__valute">
+            <span className="chart__header__valute__icon" />
+            <span className="chart__header__valute__icon" />
+            <span className="chart__header__valute__text">
+              WNBN / WNBN
+            </span>
+          </div>
+          <span className="chart__header__current__text">
+            556.7373732987
+          </span>
+          <span className="chart__header__current__change">
+            +0.00018
+          </span>
+        </div>
+      </div>
       <CandleStickChart />
     </div>
   );
