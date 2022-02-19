0595a5598295fbfa6fc1c8973ee2c0f6ef383ad1
SergeyBeltsin <beltsins89@gmail.com>
2022-02-19 12:35:48 +0300

    style fixes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -23,97 +23,102 @@ export const Header: FC = () => {
   useOutsideAlerter(headerRef, () => setIsOpened(false));
 
   return (
-    <div ref={headerRef}>
-      <header
-        className={classnames('header', {
-          'header--opened': isOpened,
-        })}
-      >
-        <div className="header__logo">
-          <img src="/assets/logo.png" alt="" width={220} />
-        </div>
-        <nav className="header__nav nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <NavLink to="/staking" onClick={handleOpen}>
-                Staking
-              </NavLink>
+    <>
+      <div className="header__mobile-logo">
+        <img src="/assets/logo.png" alt="" width={100} />
+      </div>
+      <div ref={headerRef}>
+        <header
+          className={classnames('header', {
+            'header--opened': isOpened,
+          })}
+        >
+          <div className="header__logo">
+            <img src="/assets/logo.png" alt="" width={220} />
+          </div>
+          <nav className="header__nav nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink to="/staking" onClick={handleOpen}>
+                  Staking
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/portfolio" onClick={handleOpen}>
+                  Portfolio
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/lmx" onClick={handleOpen}>
+                  Lock LMX
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/" onClick={handleOpen}>
+                  Trade
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <ul className="header__social__list">
+            <li className="header__social__item">
+              <a href="#" className="header__social__link">
+                <TwitterIcon />
+              </a>
             </li>
-            <li className="nav__item">
-              <NavLink to="/portfolio" onClick={handleOpen}>
-                Portfolio
-              </NavLink>
+            <li className="header__social__item">
+              <a href="#" className="header__social__link">
+                <TelegramIcon />
+              </a>
             </li>
-            <li className="nav__item">
-              <NavLink to="/lmx" onClick={handleOpen}>
-                Lock LMX
-              </NavLink>
+            <li className="header__social__item">
+              <a href="#" className="header__social__link">
+                <MDownIcon />
+              </a>
             </li>
-            <li className="nav__item">
-              <NavLink to="/" onClick={handleOpen}>
-                Trade
-              </NavLink>
+            <li className="header__social__item">
+              <a href="#" className="header__social__link">
+                <GithubIcon />
+              </a>
             </li>
           </ul>
-        </nav>
-        <ul className="header__social__list">
-          <li className="header__social__item">
-            <a href="#" className="header__social__link">
-              <TwitterIcon />
-            </a>
-          </li>
-          <li className="header__social__item">
-            <a href="#" className="header__social__link">
-              <TelegramIcon />
-            </a>
-          </li>
-          <li className="header__social__item">
-            <a href="#" className="header__social__link">
-              <MDownIcon />
-            </a>
-          </li>
-          <li className="header__social__item">
-            <a href="#" className="header__social__link">
-              <GithubIcon />
-            </a>
-          </li>
-        </ul>
+          <button
+            className="header__close"
+            type="button"
+            onClick={handleOpen}
+            aria-label="close navigation"
+          />
+          <div className="header__user-nav user-nav">
+            <button type="button" className="user-nav__mainnet">
+              <MainnetIcon />
+              <span>DCS Mainnet</span>
+              <DropdownArrowIcon />
+            </button>
+            <div className="user-nav__balance">
+              <span className="user-nav__balance__icon" />
+              0
+            </div>
+            <div className="user-nav__balance">
+              <span className="user-nav__balance__icon" />
+              0.0137 BNB
+            </div>
+            <span className="user-nav__wallet">
+              0x039e...6e37
+            </span>
+            <button className="user-nav__settings" type="button">
+              <SettingsIcon />
+            </button>
+          </div>
+        </header>
         <button
-          className="header__close"
+          className="header__burger"
           type="button"
           onClick={handleOpen}
-          aria-label="close navigation"
-        />
-        <div className="header__user-nav user-nav">
-          <button type="button" className="user-nav__mainnet">
-            <MainnetIcon />
-            <span>DCS Mainnet</span>
-            <DropdownArrowIcon />
-          </button>
-          <div className="user-nav__balance">
-            <span className="user-nav__balance__icon" />
-            0
-          </div>
-          <div className="user-nav__balance">
-            <span className="user-nav__balance__icon" />
-            0.0137 BNB
-          </div>
-          <span className="user-nav__wallet">
-            0x039e...6e37
-          </span>
-          <button className="user-nav__settings" type="button">
-            <SettingsIcon />
-          </button>
-        </div>
-      </header>
-      <button
-        className="header__burger"
-        type="button"
-        onClick={handleOpen}
-        aria-label={isOpened ? 'close navigation' : 'open navigation'}
-      >
-        <span />
-      </button>
-    </div>
+          aria-label={isOpened ? 'close navigation' : 'open navigation'}
+        >
+          <span />
+        </button>
+      </div>
+    </>
   );
 };
